f7229cdaa8cef8230b64499d1c112c5135169379
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-13 16:46:13 -0300

    Crea clase Ficha

---
+++ gatosjpa/src/main/java/com/mycompany/gatosjpa/logica/Ficha.java
@@ -0,0 +1,66 @@
+
+package com.mycompany.gatosjpa.logica;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ficha {
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int id;
+    private boolean desparasitacion;
+    private boolean tripleFelina;
+    private boolean antirrabica;
+
+    public Ficha() {
+    }
+
+    public Ficha(int id, boolean desparasitacion, boolean tripleFelina, boolean antirrabica) {
+        this.id = id;
+        this.desparasitacion = desparasitacion;
+        this.tripleFelina = tripleFelina;
+        this.antirrabica = antirrabica;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isDesparasitacion() {
+        return desparasitacion;
+    }
+
+    public void setDesparasitacion(boolean desparasitacion) {
+        this.desparasitacion = desparasitacion;
+    }
+
+    public boolean isTripleFelina() {
+        return tripleFelina;
+    }
+
+    public void setTripleFelina(boolean tripleFelina) {
+        this.tripleFelina = tripleFelina;
+    }
+
+    public boolean isAntirrabica() {
+        return antirrabica;
+    }
+
+    public void setAntirrabica(boolean antirrabica) {
+        this.antirrabica = antirrabica;
+    }
+
+    @Override
+    public String toString() {
+        return "Ficha{" + "id=" + id + ", desparasitacion=" + desparasitacion + ", tripleFelina=" + tripleFelina + ", antirrabica=" + antirrabica + '}';
+    }
+    
+    
+}
