49fb2a28a47600205bdb8f69375f947b233a0664
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-13 18:30:04 -0300

    Crea una clase Persona

---
+++ gatosjpa/src/main/java/com/mycompany/gatosjpa/logica/Persona.java
@@ -0,0 +1,64 @@
+
+package com.mycompany.gatosjpa.logica;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Persona {
+    @Id
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Persona(String dni, String nombre, String apellido, String telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public Persona() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + '}';
+    }
+    
+    
+    
+}
