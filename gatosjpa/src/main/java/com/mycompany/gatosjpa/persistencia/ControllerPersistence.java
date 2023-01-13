59dd76fefb1c1c01c1ac6078fa225a374770b675
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-13 17:53:59 -0300

    Agrega a controllerPersistence la fichaJpaController

--- gatosjpa/src/main/java/com/mycompany/gatosjpa/persistencia/ControllerPersistence.java
+++ gatosjpa/src/main/java/com/mycompany/gatosjpa/persistencia/ControllerPersistence.java
@@ -1,6 +1,7 @@
 
 package com.mycompany.gatosjpa.persistencia;
 
+import com.mycompany.gatosjpa.logica.Ficha;
 import com.mycompany.gatosjpa.logica.Gato;
 import com.mycompany.gatosjpa.persistencia.GatoJpaController;
 import com.mycompany.gatosjpa.persistencia.exceptions.NonexistentEntityException;
@@ -11,15 +12,16 @@ import java.util.logging.Logger;
 
 
 public class ControllerPersistence {
-    GatoJpaController GatoJpa= new GatoJpaController();
+    GatoJpaController gatoJpa= new GatoJpaController();
+    FichaJpaController fichaJpa=new FichaJpaController();
 
     public void createCat(Gato cat) {
-        GatoJpa.create(cat);
+        gatoJpa.create(cat);
     }
 
     public void destroyCat(int id) {
         try {
-            GatoJpa.destroy(id);
+            gatoJpa.destroy(id);
         } catch (NonexistentEntityException ex) {
             Logger.getLogger(ControllerPersistence.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -27,19 +29,51 @@ public class ControllerPersistence {
 
     public void editCat(Gato cat) {
         try {
-            GatoJpa.edit(cat);
+            gatoJpa.edit(cat);
         } catch (Exception ex) {
             Logger.getLogger(ControllerPersistence.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public Gato bringCat(int id) {
-        return GatoJpa.findGato(id);
+        return gatoJpa.findGato(id);
     }
 
     public ArrayList<Gato> bringCats() {
-        List <Gato> list=GatoJpa.findGatoEntities();
+        List <Gato> list=gatoJpa.findGatoEntities();
         ArrayList <Gato> listCats=new ArrayList<Gato>(list);
         return listCats;
+    }
+    
+    //FICHA VETERINARIA
+
+    public void createFile(Ficha file) {
+        fichaJpa.create(file);
+    }
+
+    public void destroyFile(int id) {
+        try {
+            fichaJpa.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(ControllerPersistence.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void editFile(Ficha file) {
+        try {
+            fichaJpa.edit(file);
+        } catch (Exception ex) {
+            Logger.getLogger(ControllerPersistence.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Ficha bringFile(int id) {
+        return fichaJpa.findFicha(id);
+    }
+
+    public ArrayList<Ficha> bringFiles() {
+       List <Ficha> list=fichaJpa.findFichaEntities();
+        ArrayList <Ficha> listFichas=new ArrayList<Ficha>(list);
+        return listFichas;
     }
 }
