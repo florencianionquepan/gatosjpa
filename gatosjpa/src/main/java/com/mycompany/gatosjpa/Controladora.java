cbafc86f8cde4d1ca5685fa27bb901f60a603931
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-13 12:29:21 -0300

    La clase main usa controller en vez de controllerPersistence
    Se respeta ahora el modelo de capas.

---
+++ gatosjpa/src/main/java/com/mycompany/gatosjpa/Controladora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gatosjpa;
+
+import com.mycompany.gatosjpa.logica.Controller;
+
+/**
+ *
+ * @author florencia
+ */
+public class Controladora extends Controller {
+
+    public Controladora() {
+    }
+    
+}
