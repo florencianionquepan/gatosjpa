8bd403659803ed983e2772a6b4f876b417132684
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-13 12:08:26 -0300

    crea clase controladoraPersistencia
    se encarga de administrar todas las controladoras de persistencia de todas las clases

---
+++ gatosjpa/src/main/java/com/mycompany/gatosjpa/persistencia/exceptions/ControllerPersistence.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.gatosjpa.persistencia.exceptions;
+
+import com.mycompany.gatosjpa.persistencia.GatoJpaController;
+
+
+public class ControllerPersistence {
+    GatoJpaController GatoJpa= new GatoJpaController();
+}
