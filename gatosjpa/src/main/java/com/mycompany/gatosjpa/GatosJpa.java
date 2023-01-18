6544d16cddf92391d358c642a1063732153c379b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-18 16:16:34 -0300

    Crea datos dummy

--- gatosjpa/src/main/java/com/mycompany/gatosjpa/GatosJpa.java
+++ gatosjpa/src/main/java/com/mycompany/gatosjpa/GatosJpa.java
@@ -4,7 +4,14 @@
  */
 package com.mycompany.gatosjpa;
 
-import com.mycompany.gatosjpa.persistencia.ControllerPersistence;
+import com.mycompany.gatosjpa.logica.Controller;
+import com.mycompany.gatosjpa.logica.Ficha;
+import com.mycompany.gatosjpa.logica.Gato;
+import com.mycompany.gatosjpa.logica.Persona;
+import com.mycompany.gatosjpa.logica.Voluntario;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
 
 /**
  *
@@ -17,7 +24,56 @@ public class GatosJpa {
      */
     public static void main(String[] args) {
         //De esta forma se van a crear todas las tablas que tenga hasta el momento
-        ControllerPersistence controlPersis=new ControllerPersistence();
+        Controller control= new Controller();
+        
+        /*
+        Gato cat;
+        cat = new Gato(2,"Ateneo",new ArrayList(),"macho","Gatito sobreviviente","raza","negro",false, new Date());
+        control.createCat(cat);
+        
+        cat.setNombre("igorio");
+        control.editCat(cat);
+        control.destroyCat(2);
+        */
+        
+        //Gato catBuscado=control.bringCat(3);
+        //System.out.println(catBuscado.toString());
+        
+        /*ArrayList <Gato> listaGatitos=control.bringCats();
+        for(int i=0;i<listaGatitos.size();i++){
+            System.out.println(listaGatitos.get(i).toString());
+        }
+        
+        */
+        //1)CREAR LISTA DE GATOS VACIA
+        LinkedList <Gato> listaGatis=new LinkedList<Gato>();
+        
+        //2)CREAMOS UNA PERSONA Y UN VOLINTARIO
+        //Persona Florencia=new Persona("36192394","Flower","Nonque","4143822");
+        
+        //3)CREO VOLUNTARIO
+        Voluntario volunt=new Voluntario("36192394","Flower","Nonque","4143822",new Date(),listaGatis);
+        control.createVol(volunt);
+        
+        //4)Creo los gatos a cargo. 
+        Ficha fichaVet1=new Ficha(1,true,false,false);
+        control.createFile(fichaVet1);
+        Ficha fichaVet2=new Ficha(2,true,false,false);
+        control.createFile(fichaVet2);
+        
+        Gato catEmma=new Gato(6,"Emma",new ArrayList(),"hembra","Gatita loca","pardo","naranja",false, new Date(),fichaVet1,volunt);
+        control.createCat(catEmma);
+        Gato catIgor=new Gato(7,"Igor",new ArrayList(),"macho","Gatita loca","pardo","negro",true, new Date(),fichaVet2,volunt);
+        control.createCat(catIgor);
+        
+        listaGatis.add(catEmma);
+        listaGatis.add(catIgor);
+        //System.out.println("----GATA LOCA----");
+        //Gato cat=control.bringCat(1);
+        //System.out.println("La gata: "+cat.getNombre()+" posee la ficha "+cat.getFichaVet().toString());
+        volunt.setGatos(listaGatis);
+        control.editVol(volunt);
+        
     }
     
 }
