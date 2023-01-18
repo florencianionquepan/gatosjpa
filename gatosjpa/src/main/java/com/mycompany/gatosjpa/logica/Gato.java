a8b01a51affef3be2095de2a6996a9eca82c49d1
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-01-18 16:15:49 -0300

    Agrega voluntario como atributo de gato

--- gatosjpa/src/main/java/com/mycompany/gatosjpa/logica/Gato.java
+++ gatosjpa/src/main/java/com/mycompany/gatosjpa/logica/Gato.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -32,10 +33,12 @@ public class Gato implements Serializable {
     @OneToOne
     private Ficha fichaVet;
     
+    @ManyToOne
+    private Voluntario volunt;
     public Gato() {
     }
 
-    public Gato(int id, String nombre, ArrayList<String> srcFoto, String sexo, String descripcion, String raza, String color, boolean adoptado, Date fechaNac, Ficha fichaVet) {
+    public Gato(int id, String nombre, ArrayList<String> srcFoto, String sexo, String descripcion, String raza, String color, boolean adoptado, Date fechaNac, Ficha fichaVet, Voluntario volunt) {
         this.id = id;
         this.nombre = nombre;
         this.srcFoto = srcFoto;
@@ -46,7 +49,18 @@ public class Gato implements Serializable {
         this.adoptado = adoptado;
         this.fechaNac = fechaNac;
         this.fichaVet = fichaVet;
+        this.volunt = volunt;
     }
+
+    public Voluntario getVolunt() {
+        return volunt;
+    }
+
+    public void setVolunt(Voluntario volunt) {
+        this.volunt = volunt;
+    }
+
+    
     
     public int getId() {
         return id;
